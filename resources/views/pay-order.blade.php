da534050af1820501881f6aa0a4804dda7534303
Anwar Shaikh <anwar.squarebiz@gmail.com>
2026-04-06 00:47:41 +0530

    changed the logo

--- resources/views/pay-order.blade.php
+++ resources/views/pay-order.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>
         @if($order->payment_type?->value === 'inr')
